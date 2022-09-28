479717d5a79e8b22cc168b3a846affd754ea9eb5
ArmandLedoux <89807353+ArmandLedoux@users.noreply.github.com>
2022-09-28 10:11:49 +0200

    Update decodage.py

--- code/decodage.py
+++ code/decodage.py
@@ -1,11 +1,11 @@
 def concat (l1, l2) :
     for i in l2 :
         l1.append(i)
-def assemble (lll) :
-    """takes in argument the list of all the lists that contains 64 lists of 57 elements"""
+def assemble(lll) :
+    """lll = [matrices de 64*57]
+    Hamming a corrigé les erreurs et retiré les bits de parité, maintenant on recompose la liste de bits."""
     sequence = []
     for ll in lll :
-        print(ll)
         for l in ll :
             concat(sequence, l)
     return sequence
@@ -21,3 +21,15 @@ def assemble (lll) :
 #             print (l)
 #         print(';')
 # -------------------------------------------------------------------------------------
+assemble()
+
+def hamming (lll) :
+    """lll = [matrices de 64*64]
+    On a lu les qr codes et formé nos paquets de 64 bits, 
+    maintenant on corrige les potentielles erreurs et on retire les bits de parité."""
+    pass
+
+def lecture (lqr) : 
+    """on a analysé l'image de qr code et rempli une liste du contenu du qr code, donc maint """
+    iqr = 0 # numéro du qr code
+    
