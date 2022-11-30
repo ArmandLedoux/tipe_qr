9d82de4c08ca5641d4f429ac476ab756df1a780a
ArmandLedoux <89807353+ArmandLedoux@users.noreply.github.com>
2022-11-30 10:26:27 +0100

    Update decodage.py

--- code/decodage.py
+++ code/decodage.py
@@ -1,27 +1,51 @@
-def formation_des_paquets (lqr, N=64, n=64) : 
+def zone_a_lire (x, y, n) :
+    if x < 8 and y < 8 :
+        return False
+    elif x < 8 and y >n-9 :
+        return False
+    elif x > n-9 and y<8 :
+        return False
+    else : 
+        return True
+
+def lecture_matrice (llqr) :
+    taille_qr = len(llqr[0])
+    lqr = []
+    for iqr in range(len(llqr)) :
+        qr = []
+        for x in range(taille_qr) :
+            for y in range(taille_qr) :
+                if zone_a_lire(x,y,taille_qr) :
+                    qr.append(llqr[iqr][x][y])
+        lqr.append(qr)
+    return lqr
+
+def formation_des_paquets (lqr, nb_mat, nb_cor) : 
     """après lecture du qr code, on a créé une liste contenant chacun des qr codes sous forme de listes.
-    on les convertit en une liste de paquets contenant N grilles à n élements pour hamming"""
-    llpaquet = [] # liste des paquets
-    lpaquet = [] # contient un paquet de taille 64*64 = 4096
+    on les convertit en une liste de paquets contenant nb_mat matrices de correction d'erreur,
+    chacune comprenant nb_cor+1 bits de correction d'erreur (soit de taille 1<<nb_cor)"""
+    n = 1<<nb_cor
+    lpaquet = [] # liste des paquets
+    paquet = [] # contient un paquet de taille n*nb_mat
     i = 0 # pour repérer où on est dans le paquet
     for qr in lqr :
         for j in range(len(qr)) :
-            lpaquet.append(qr[j])
+            paquet.append(qr[j])
             i+=1
-            if i == n*N : # on a terminé un paquet
+            if i == nb_mat*n : # on a terminé un paquet
                 i = 0
-                llpaquet.append(lpaquet) 
-                lpaquet = []
-    lll = [formation(lpaquet,N,n) for lpaquet in llpaquet]
+                lpaquet.append(paquet) 
+                paquet = []
+    lll = [formation(paquet,nb_mat,n) for paquet in lpaquet]
     return lll 
-    # si on a un lpaquet non vide à la fin de l'algo, c'est qu'on a créé un début de lpaquet pour remplir le dernier qr code
 
-def formation (lpaquet, N, n) :
+def formation (paquet, nb_mat, n) :
+    """prépare les paquets de matrices de correction d'erreur pour hamming"""
     ll = []
-    for i in range (N) :
+    for i in range (nb_mat) :
         l = []
         for j in range(n) :
-            l.append(lpaquet[j*64+i])
+            l.append(paquet[j*n+i])
         ll.append(l)
     return ll
 
@@ -45,29 +69,30 @@ def formation (lpaquet, N, n) :
 
 
 
-def correction_d_erreur (lll) :
+def correction_d_erreur (lll, nb_cor) :
     """lll = [matrices de 64*64]
     On a lu les qr codes et formé nos paquets de 64 bits, 
     maintenant on corrige les potentielles erreurs"""
 
-    def hamming_decode(l, n=3) : # on code pour une taille générale au cas où l'efficacité d'une matrice 8x8 serait mauvaise
+    def hamming_decode(l, nb_cor) : # on code pour une taille générale au cas où l'efficacité d'une matrice 8x8 serait mauvaise
         """D'après hamming, l'erreur, si elle existe et si elle est seule,
         se trouve aux coordonnées fournies par le xor de tous les bits impairs
         (les bits de parités sont là pour s'assurer ce résultat,
         via une méthode dichotomique sur les lignes et les colonnes)"""
-        x = 0
+        xor = 0
         tot = 0
-        for k in range (1, 1<<(2*n)):
+        for k in range (1, 1<<(nb_cor)):
             if l[k] == 1 :
-                x = x ^ k
-                tot = (tot + 1)
-        if tot % 2 == l[0] and x != 0:
-            print ("Au moins deux erreurs")
+                xor = xor ^ k
+                tot += 1
+        if tot % 2 == l[0] and xor != 0:
+            # print ("Au moins deux erreurs")
+            pass
         else :
-            l[x] = 1-l[x]
+            l[xor] = 1-l[xor]
     for ll in lll :
         for l in ll :
-            hamming_decode(l)
+            hamming_decode(l, nb_cor)
     
 
 
@@ -95,8 +120,13 @@ def retire_bits_de_parite(l) :
 
 
 def assemble(lll) :
-    """lll = [matrices de 64*57]
-    Hamming a corrigé les erreurs et retiré les bits de parité, maintenant on recompose la liste de bits."""
+    """lll = [matrices de 64*64]
+    Hamming a corrigé les erreurs, maintenant on retire les bits de parité recompose la liste de bits."""
+    for a in range(len(lll)) :
+        ll = lll[a]
+        for b in range(len(ll)) :
+            ll[b] = retire_bits_de_parite(ll[b])
+    
     def concat (l1, l2) :
         for i in l2 :
             l1.append(i)
@@ -104,11 +134,22 @@ def assemble(lll) :
     for ll in lll :
         for l in ll :
             concat(sequence, l)
-    return sequence
+    # return sequence
     n = len(sequence) 
+    # temp = ""
+    # for i in range(n) :
+    #     temp+=str(sequence[i])
+    # print(temp, ";")
+    # print("-------------------------------------------")
+    compteur = 0
+    # temp = ''
     for i in range(n-1,-1,-1) :
+        compteur +=1
         if sequence[i] != 0 :
-            return sequence[:i-1]
+            # for j in range (i-50, i+50) :
+            #     temp+=str(sequence[j])
+            # print(temp)
+            return sequence[:i]
         
     
 
@@ -125,3 +166,16 @@ def assemble(lll) :
 # -------------------------------------------------------------------------------------
 
 
+
+def decodage (llqr,nb_mat=64,nb_cor=6) :
+    lqr = lecture_matrice(llqr)
+    lll = formation_des_paquets (lqr, nb_mat, nb_cor)
+    correction_d_erreur (lll,nb_cor)
+    return assemble(lll)
+
+
+# llqr = [[[randint(0,1) for i in range(64)] for j in range(64)] for i in range(5)]
+
+# print(decodage(llqr))
+
+# print("ok")
